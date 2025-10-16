c5f64dc347e822ee0a09a6decc2e9aa3f8b9a956
Biditchoun <sawijnbe@shi-r4-p7.s19.be>
2025-10-16 20:32:35 +0200

    Corrected a problem with \0

--- Piscine_reloaded/ex27/main.c
+++ Piscine_reloaded/ex27/main.c
@@ -6,7 +6,7 @@
 /*   By: sawijnbe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 20:19:53 by sawijnbe          #+#    #+#             */
-/*   Updated: 2025/10/14 21:21:04 by sawijnbe         ###   ########.fr       */
+/*   Updated: 2025/10/16 20:21:20 by sawijnbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ int	str_len(char *str)
 	return (i);
 }
 
-int	write_and_return(char *str, int fd, int i)
+int	write_and_return(char *str, int len, int fd, int i)
 {
-	write(fd, str, str_len(str));
+	write(fd, str, len);
 	return (i);
 }
 
@@ -37,12 +37,12 @@ int	arg_check(int ac, char **av)
 
 	rt = 0;
 	if (ac < 2)
-		return (write_and_return("File name missing.\n", 2, 0));
+		return (write_and_return("File name missing.\n", 19, 2, 0));
 	else if (ac > 2)
-		return (write_and_return("Too many arguments.\n", 2, 0));
+		return (write_and_return("Too many arguments.\n", 20, 2, 0));
 	rt = open(av[1], O_RDONLY);
 	if (rt < 1)
-		return (write_and_return("Cannot read file.\n", 2, 0));
+		return (write_and_return("Cannot read file.\n", 18, 2, 0));
 	return (rt);
 }
 
@@ -54,15 +54,14 @@ int	main(int ac, char **av)
 
 	fd = arg_check(ac, av);
 	if (!fd)
-		return (0);
+		return (1);
 	read_rt = 1000;
 	while (read_rt == 1000)
 	{
 		read_rt = read(fd, buff, 1000);
 		if (read_rt < 0)
-			return (write_and_return("Cannot read file.\n", 2, 0));
-		buff[read_rt] = 0;
-		write_and_return(buff, 1, 0);
+			return (write_and_return("Cannot read file.\n", 18, 2, 0));
+		write_and_return(buff, read_rt, 1, 0);
 	}
 	close(fd);
 	return (0);
